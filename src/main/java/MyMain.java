d255b4d7224b79e8694c85e3936d84cbdc1ee982
Nathan Lin <nlin@athenian.org>
2022-01-10 10:40:16 -0800

    first drafty

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -3,13 +3,29 @@ public class MyMain {
     // Returns the number of odd numbers in mat
     public static int countOdds(int[][] mat) {
         // YOUR CODE HERE
-        return -1;
+        int num = 0;
+        for (int row = 0; row < mat.length; row++) {
+            for (int col = 0; col < mat[0].length; col++) {
+                if (mat[row][col] % 2 != 0) {
+                    num++;
+                }
+            }
+        }
+        return num;
     }
 
     // Returns the largest value in mat
     public static int findLargest(int[][] mat) {
         // YOUR CODE HERE
-        return -1;
+        int x = Integer.MIN_VALUE;
+        for (int row = 0; row < mat.length; row++) {
+            for (int col = 0; col < mat[0].length; col++) {
+                if (mat[row][col] > x) {
+                    x = mat[row][col];
+                }
+            }
+        }
+        return x;
     }
 
     // Write a method that "left-shifts" each row in a 2D array.
@@ -18,14 +34,27 @@ public class MyMain {
     // should wrap around to the last entry in the row)
     public static int[][] leftShift(int[][] mat) {
         // YOUR CODE HERE
-        return null;
+        for (int row = 0; row < mat.length; row++) {
+            int first = mat[row][0];
+            for (int col = 0; col < mat[0].length - 1; col++) {
+                mat[row][col] = mat[row][col+1];
+            }
+            mat[row][mat[row].length-1] = first;
+        }
+        return mat;
     }
 
     // Creates a new array list of size arraySize x arraySize
     // where each value is each to the sum of its row and col indexes
     public static int[][] arrayBuilder(int arraySize) {
         // YOUR CODE HERE
-        return null;
+        int[][] x = new int[arraySize][arraySize];
+        for (int row = 0; row < x.length; row++) {
+            for (int col = 0; col < x[0].length; col++) {
+                x[row][col] = row + col;
+            }
+        }
+        return x;
     }
 
 
@@ -41,23 +70,68 @@ public class MyMain {
 
     // Returns the mean of the 2D array mat
     public static double mean(double[][] mat) {
-        // YOUR CODE HERE
-        return -1.0;
+        double sum = 0;
+        int count = 0;
+        for (int row = 0; row < mat.length; row++){
+            for (int col = 0; col < mat[0].length; col++){
+                sum = sum + mat[row][col];
+                count++;
+            }
+        }
+        return sum/count;
     }
 
     // Returns the median of the 2D array mat
-    // Remember that the array is sorted!
     public static double median(double[][] mat) {
-        // YOUR CODE HERE
-        return -1.0;
+        int count = 0;
+        int count2 = 0;
+        for (int row = 0; row < mat.length; row++){
+            for (int col = 0; col < mat[0].length; col++){
+                for (int i = 0; i < mat.length * mat[0].length; i++){
+                    count ++;
+                }}}
+        double[] arr = new double[count];
+        for (int row = 0; row < mat.length; row++){
+            for (int col = 0; col < mat[0].length; col++){
+                for (int i = 0; i < mat.length * mat[0].length; i++){
+                    arr[count2] = mat[row][col];
+                    count2 ++; }}}
+
+        if (count % 2 == 0){
+            return (double)(arr[(int)count/2] + arr[(int)count/2-1])/2;
+        }
+
+        else {
+            return (Math.round(arr[count/2]));}
+
     }
 
 
     // Returns the mode of the 2D array mat
-    // Remember that the array is sorted!
     public static double mode(double[][] mat) {
-        // YOUR CODE HERE
-        return -1.0;
+        int count = 0;
+        int most = 0;
+        double mode = 0;
+        double[] arr = new double[mat.length * mat[0].length];
+        double[] arr2 = new double[mat.length * mat[0].length];
+        for (int row = 0; row < mat.length; row++){
+            for (int col = 0; col < mat[0].length; col++){
+                arr[count] = mat[row][col];
+                arr2[count] = mat[row][col];
+                count++;
+            }}
+        for (int row = 0; row < mat.length; row++){
+            for (int col = 0; col < mat[0].length; col++){
+                for (int i = 0; i < mat.length * mat[0].length; i++){
+                    count ++;
+                }}}
+
+        for(int i = 0; i < arr.length-1; i++){
+            int equal = 0;
+            if (arr[i] == arr[i+1]){equal++;}
+            if (equal > most){equal = most; mode = arr[i];}
+        }
+        return mode;
     }
 
 
